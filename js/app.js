3334458e265966cf331e5fecb7c94096b93bbf5c
AandEs <edouard@aandes.com>
2017-06-21 14:48:10 -0400

    Refactor for compatibility with RAPID v0.4.001

--- js/app.js
+++ js/app.js
@@ -6,15 +6,15 @@
 
             return document.querySelector("head script[src*='/~rapid/edit/']") !== null
             && (typeof rapidBeta === 'function')
-            && !!rapidBeta('project.data.cms.page.getResourcePath');
+            && !!rapidBeta('mirror.data.cms.page.getResourcePath');
 
         },
 
         getContent: function (view) {
-
+            
             if (api.authoring()) {
 
-                return $.getJSON(rapidBeta('project.data.cms.page.getResourcePath')() + '.infinity.json');
+                return $.getJSON(rapidBeta('mirror.data.cms.page.getResourcePath')() + '.infinity.json');
 
             }
             
